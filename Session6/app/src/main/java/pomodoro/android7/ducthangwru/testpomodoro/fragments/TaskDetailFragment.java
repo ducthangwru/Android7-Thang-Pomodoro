d3fdcffde20dad19f685700e3eb6ccdf7b5b0043
DESKTOP-TVC0PFL\DUC THANG <ducthangwru97@gmail.com>
2017-02-12 13:11:04 +0700

    session 6

---
+++ Session6/app/src/main/java/pomodoro/android7/ducthangwru/testpomodoro/fragments/TaskDetailFragment.java
@@ -0,0 +1,56 @@
+package pomodoro.android7.ducthangwru.testpomodoro.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import pomodoro.android7.ducthangwru.testpomodoro.R;
+import pomodoro.android7.ducthangwru.testpomodoro.adapters.ColorAdapter;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TaskDetailFragment extends Fragment {
+    private ColorAdapter colorAdapter = new ColorAdapter();
+    @BindView(R.id.rv_color)
+    RecyclerView rv_color;
+
+    public TaskDetailFragment() {
+        // Required empty public constructor
+        setHasOptionsMenu(true);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_task_detail, container, false);
+        setupUI(view);
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_edit_task, menu);
+    }
+
+    private void setupUI(View view) {
+        ButterKnife.bind(this, view);
+        rv_color.setAdapter(colorAdapter);
+        rv_color.setLayoutManager(new GridLayoutManager(this.getContext(), 4));
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this.getContext(), DividerItemDecoration.VERTICAL);
+        rv_color.addItemDecoration(dividerItemDecoration);
+    }
+}
---
+++ Session6/app/src/main/java/pomodoro/android7/ducthangwru/testpomodoro/fragments/TaskFragment.java
@@ -0,0 +1,48 @@
+package pomodoro.android7.ducthangwru.testpomodoro.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import pomodoro.android7.ducthangwru.testpomodoro.R;
+import pomodoro.android7.ducthangwru.testpomodoro.activities.TaskActivity;
+import pomodoro.android7.ducthangwru.testpomodoro.adapters.TaskAdapter;
+
+public class TaskFragment extends Fragment {
+    @BindView(R.id.rv_task)
+    RecyclerView rvTask;
+    TaskAdapter taskAdapter;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View  view = inflater.inflate(R.layout.fragment_task, container, false);
+        setupUI(view);
+        return view;
+    }
+
+    private void setupUI(View view) {
+        ButterKnife.bind(this, view);
+        taskAdapter = new TaskAdapter();
+        rvTask.setAdapter(taskAdapter);
+        rvTask.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this.getContext(), DividerItemDecoration.VERTICAL);
+        rvTask.addItemDecoration(dividerItemDecoration);
+    }
+
+    @OnClick(R.id.fab)
+    void onFabClick() {
+        ((TaskActivity)getActivity()).replaceFragment(new TaskDetailFragment(), true);
+    }
+}
